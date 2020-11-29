27da0cf49ad05a2bebcf0a8f174029291af81b00
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-29 20:35:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,9 +6,8 @@ def join_nested_strings(src)
   while row_index < src.length do
     element_index = 0 
     while element_index < src[row_index].length do
-      while src[row_index][element_index].class == String do 
+      if src[row_index][element_index].class == String do 
         full_str += src[row_index][element_index] + " "
-    end 
     element_index += 1 
     end 
     row_index += 1 
